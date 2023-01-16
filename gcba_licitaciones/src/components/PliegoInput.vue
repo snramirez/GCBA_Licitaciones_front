a3a4cd97b15253c9e69e925bd524d823e9031ebc
snramirez <54188400+snramirez@users.noreply.github.com>
2023-01-15 23:25:57 -0300

    Update PliegoInput.vue

--- gcba_licitaciones/src/components/PliegoInput.vue
+++ gcba_licitaciones/src/components/PliegoInput.vue
@@ -339,11 +339,30 @@
         </v-col>
 
         <v-col cols="12" md="3">
-          <v-text-field
-            v-model="bidding.DayQuantity"
-            label="Cantidad de dias"
-            :counter="64"
-          ></v-text-field>
+          <v-menu
+            v-model="menu9"
+            :close-on-content-click="false"
+            :nudge-right="40"
+            transition="scale-transition"
+            offset-y
+            min-width="auto"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-text-field
+                v-model="bidding.SecondLapPG"
+                label="2da vuelta de PG"
+                prepend-icon="mdi-calendar"
+                readonly
+                v-bind="attrs"
+                v-on="on"
+              ></v-text-field>
+            </template>
+            <v-date-picker
+              v-model="bidding.SecondLapPG"
+              @input="menu9 = false"
+              locale="es-AR"
+            ></v-date-picker>
+          </v-menu>
         </v-col>
 
         <v-col cols="12" md="3">
